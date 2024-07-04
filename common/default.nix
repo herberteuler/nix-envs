fe9375bedac43599b3b4bd17d5f177ac61f9aae4
Guanpeng Xu <herberteuler@gmail.com>
2024-07-03 21:28:50 -0700

    Add an environment for working on Kotlin's source code

---
+++ common/default.nix
@@ -0,0 +1,23 @@
+{ pkgs
+, name
+}:
+
+let
+
+  bashrcLocal = let
+    sgr0 = "\\[\\$(tput sgr0)\\]";
+    hi-blue = "\\[\\e[1;34m\\]";
+    hi-white = "\\[\\e[38;5;15m\\]";
+    hi-yellow = "\\[\\e[38;5;228m\\]";
+  in pkgs.runCommandLocal "bashrc-local" { } ''
+          mkdir -p $out/etc
+          cat >$out/etc/bashrc.local <<EOF
+export EMACS_ENV="Programs-Kotlin"
+export PS1="(${sgr0}${hi-white}${name}${sgr0}) [${sgr0}${hi-yellow}\w${sgr0}] ${sgr0}${hi-blue}\\$ ${sgr0}"
+unset TZ
+EOF
+        '';
+
+in {
+  inherit bashrcLocal;
+}
---
+++ kotlin/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "The environment for Kotlin";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    devShells.x86_64-linux.default = let
+
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      name = "kotlin";
+
+      common = import ../common { inherit pkgs name; };
+
+    in (pkgs.buildFHSEnv {
+      inherit name;
+      profile = ''
+        source /etc/bashrc.local
+      '';
+      targetPkgs = pkgs: with pkgs; [
+        common.bashrcLocal jdk8
+      ];
+    }).env;
+  };
+}
